6c08edb444503a4fae1059b517caa2cc29c79406
waheedanr310 <waheedanr310@gmail.com>
2022-12-29 18:50:11 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('exoress')
+const app = express()
+const port = 8000
+app.listen(port, ()=>{
+    console.log( `Server is running ${port}`)
+})
+console.log('Waheed')
+console.log(module)
+console.log('Waheed')
